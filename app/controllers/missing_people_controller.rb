986b1923b52aec2205e8e18bd2a799d6e6324789
Bruno Tavares <bruno.exz@gmail.com>
2013-01-28 21:54:41 -0200

    Basic missing person functionality

--- app/controllers/missing_people_controller.rb
+++ app/controllers/missing_people_controller.rb
@@ -1,8 +1,11 @@
 class MissingPeopleController < ApplicationController
+
+  before_filter :set_event
+
   # GET /missing_people
   # GET /missing_people.json
   def index
-    @missing_people = MissingPerson.all
+    @missing_people = @event.missing_people.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,7 +16,7 @@ class MissingPeopleController < ApplicationController
   # GET /missing_people/1
   # GET /missing_people/1.json
   def show
-    @missing_person = MissingPerson.find(params[:id])
+    @missing_person = @event.missing_people.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -24,7 +27,7 @@ class MissingPeopleController < ApplicationController
   # GET /missing_people/new
   # GET /missing_people/new.json
   def new
-    @missing_person = MissingPerson.new
+    @missing_person = @event.missing_people.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -34,17 +37,17 @@ class MissingPeopleController < ApplicationController
 
   # GET /missing_people/1/edit
   def edit
-    @missing_person = MissingPerson.find(params[:id])
+    @missing_person = @event.missing_people.find(params[:id])
   end
 
   # POST /missing_people
   # POST /missing_people.json
   def create
-    @missing_person = MissingPerson.new(params[:missing_person])
+    @missing_person = @event.missing_people.new(params[:missing_person])
 
     respond_to do |format|
       if @missing_person.save
-        format.html { redirect_to @missing_person, notice: 'Missing person was successfully created.' }
+        format.html { redirect_to @event, notice: 'Missing person was successfully created.' }
         format.json { render json: @missing_person, status: :created, location: @missing_person }
       else
         format.html { render action: "new" }
@@ -56,11 +59,11 @@ class MissingPeopleController < ApplicationController
   # PUT /missing_people/1
   # PUT /missing_people/1.json
   def update
-    @missing_person = MissingPerson.find(params[:id])
+    @missing_person = @event.missing_people.find(params[:id])
 
     respond_to do |format|
       if @missing_person.update_attributes(params[:missing_person])
-        format.html { redirect_to @missing_person, notice: 'Missing person was successfully updated.' }
+        format.html { redirect_to @event, notice: 'Missing person was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -72,12 +75,17 @@ class MissingPeopleController < ApplicationController
   # DELETE /missing_people/1
   # DELETE /missing_people/1.json
   def destroy
-    @missing_person = MissingPerson.find(params[:id])
+    @missing_person = @event.missing_people.find(params[:id])
     @missing_person.destroy
 
     respond_to do |format|
-      format.html { redirect_to missing_people_url }
+      format.html { redirect_to event_url(@event) }
       format.json { head :no_content }
     end
+  end
+
+  private
+  def set_event
+    @event = Event.find params[:event_id]
   end
 end
